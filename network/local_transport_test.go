bf1e13cfc2e3ed39caac36532b63702b6d14149c
indolab <suyashm480@gmail.com>
2024-01-22 18:40:41 +0530

    modified:   network/local_transport_test.go

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -16,3 +16,17 @@ func TestConnect(t *testing.T) {
 	assert.Equal(t, tra.peers[trb.addr], trb)
 	assert.Equal(t, trb.peers[tra.addr], tra)
 }
+
+func TestSendMessage(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+
+	tra.Connect(trb)
+	trb.Connect(tra)
+	msg := []byte("hello!")
+	assert.Nil(t, tra.SendMessage(tra.addr, msg))
+
+	rpc := <-trb.Consume()
+	assert.Equal(t, rpc.Payload, msg)
+	assert.Equal(t, rpc.From, tra.addr)
+}
